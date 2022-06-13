69a83878533ddfd8123dccfa8acb34470a0add5e
TommasoCanc <tommaso.cancellario@gmail.com>
2022-06-13 14:11:16 +0200

    Create Start_App.R

---
+++ Shiny_LT Env Data Analysis/Start_App.R
@@ -0,0 +1,8 @@
+####################################
+# START Shiny LT Env Data Analysis #
+####################################
+
+library(shiny)
+runGitHub('SIMILE','TommasoCanc', ref="main", subdir = "Shiny_LT Env Data Analysis")
+
+
