0b3c27c0a84ce423a582aea521e72d061fb277a2
TommasoCanc <tommaso.cancellario@gmail.com>
2022-06-22 09:34:08 +0200

    Update server.R

--- Shiny_LT Env Data Analysis/server.R
+++ Shiny_LT Env Data Analysis/server.R
@@ -1,7 +1,7 @@
 # Shiny Server Side -------
 server <- function(input, output, session) {
 
-  source("./SERVER/01_dashData_SERVER_VRE.R.R", local = T) 
+  source("./SERVER/01_dashData_SERVER_VRE.R", local = T) 
   source("./SERVER/02_condSelection_SERVER.R", local = T) 
   source("./SERVER/03_condPlot_SERVER.R", local = T) 
   
