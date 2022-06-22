f910df2f6553590ce5243a4a0f4099ec7582f8c0
TommasoCanc <tommaso.cancellario@gmail.com>
2022-06-22 12:05:22 +0200

    2022_06_22

---
+++ Shiny_LT Env Data Analysis/UI/00_dashHome_UI_VRE.R
@@ -0,0 +1,22 @@
+fluidRow(
+
+##############
+# Right side #
+##############
+
+column(width = 4,
+box(title = "The Application", width = 12,
+HTML("Hello world!"),
+hr(),
+img(src='Simile.png')
+)
+),
+
+#############
+# Left side #
+#############
+column(width = 8,
+box(title = "The Application information", width = 12,
+HTML("Hello world!"))
+)
+)
